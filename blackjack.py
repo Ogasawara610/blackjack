0311ca46fb609ea9a04dfc48fc58e734ca4a1aba
Ogasawara610 <64236032+Ogasawara610@users.noreply.github.com>
2020-04-27 12:17:57 +0900

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -1,11 +1,23 @@
 import random
 import sys
 
+rank = 0
+def get_point(hand):
+    result = 0
+    for card in hand:
+        if card[rank] > 10:
+            num = 10
+        else:
+            num = card[rank]
+        result = result + num
+    return result
+
 def all_card():
     marks = ["♠","♧","♡","♦"]
-    num = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+    num = range(1,14)
     trump = [(x,y) for x in num for y in marks]
     random.shuffle(trump)
+    print(trump)
     return trump
 
 def main():
@@ -20,34 +32,26 @@ def main():
     player_hand.append(card)
     card = trump.pop()  #P2,D2
     dealer_hand.append(card)
-    player_card = []
-    dealer_card = []
     player_time = 2
     dealer_time = 2
-
-    for x in range(0,player_time):
-        player_card.append(player_hand[x][0])
-    player_sum = sum(player_card)
-    for xx in range(0,dealer_time):
-        dealer_card.append(dealer_hand[xx][0])
-    dealer_sum = sum(dealer_card)
+    player_sum = get_point(player_hand)
+    dealer_sum = get_point(dealer_hand)
 
     print("プレイヤーは"+str(player_hand[0][1])+"の"+str(player_hand[0][0])+"と"+str(player_hand[1][1])+"の"+str(player_hand[1][0])+"の合計"+str(player_sum)+"")
-    print("ディーラーは"+str(dealer_hand[0][1])+"の"+str(dealer_hand[0][0])+"です")
+    print("ディーラーは"+str(dealer_hand[0][1])+"の"+str(dealer_hand[0][0])+"")
 
     while player_sum <21:
         choice = input("カードを引きますか yes/no:")
         if choice in ("yes","Yes","y"):
             print("プレイヤーがもう一枚引く")
             card = trump.pop() #P2+
-            player_hand.append(card)
             player_time = player_time + 1
-            player_card.append(player_hand[player_time-1][0])
-            player_sum = sum(player_card)
+            player_hand.append(card)
+            player_sum = get_point(player_hand)
             print("プレイヤーの持ちカードは")
             for y in range(0,player_time):
                 print(""+str(player_hand[y][1])+"の"+str(player_hand[y][0])+"と")
-            print("の合計"+str(player_sum)+"")
+            print("合計"+str(player_sum)+"")
             if player_sum > 21:
                 print("burst")
                 print("プレイヤーの負け")
@@ -61,8 +65,7 @@ def main():
         card = trump.pop() #D2+
         dealer_hand.append(card)
         dealer_time = dealer_time + 1
-        dealer_card.append(dealer_hand[dealer_time-1][0])
-        dealer_sum = sum(dealer_card)
+        dealer_sum = get_point(dealer_hand)
         print("ディーラーは")
         for yy in range(0,dealer_time):
             print(""+str(dealer_hand[yy][1])+"の"+str(dealer_hand[yy][0])+"と")
