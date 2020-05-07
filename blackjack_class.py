77e93b13639b9efcb2a6a0fe0c1bb3116167bff4
Ogasawara610 <64236032+Ogasawara610@users.noreply.github.com>
2020-05-07 13:17:14 +0900

    ブラックジャック（オブジェクト）

---
+++ blackjack_class.py
@@ -0,0 +1,83 @@
+import random
+import sys
+
+class Deck:
+    def __inti__(self): #デッキ作り
+        self.deck = []
+        marks = ["♠","♧","♡","♦"]
+        fig = ["A","A","A","A",2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,"J","J","J","J","Q","Q","Q","Q","K","K","K","K"]
+        self.deck = [(x,y) for x in fig for y in marks]
+        random.shuffle(self.deck)
+    def draw_card(self):    #カードを引く
+        return self.deck.pop()
+
+class Card:
+    def __inti__(self,name):
+        self.name = name
+        self.time = 0
+        self.sum = 0
+        self.hand = []
+    def get_point(self,hand):    #点数計算
+        for card in hand:
+            if card == "A": return 1
+            if card == "J": return 10
+            if card == "Q": return 10
+            if card == "K": return 10
+            else:
+                fig = int(card)
+            self.sum = self.sum + fig
+            return self.sum
+    def if_display(self,display = True):
+        if display:
+            print("{}のカードは{}の{}です".format(self.name,self.hand[self.time][self.time],self.hand[self.time][self.time+1]))
+        else:
+            print("{}のカードはわかりません".format(self.name))
+    def over_twenty_one(self):
+        if self.sum > 21:
+            return True
+        return False
+
+    
+class Player(Card):
+    def is_continue(self):
+        print("{}の合計は{}".format(self.name,self.sum))
+        if input("カードを引きますか？y/n") == "y":
+            return True
+        return False
+
+class Dealer(Card):
+    def is_continue(self):
+        print("{}の合計は{}".format(self.name,self.sum))
+        if self.sum > 17:
+            return True
+        return False
+
+
+def main():
+    deck = Deck()
+    player = Player('player')
+    dealer = Dealer('dealer')
+    player.get_point(deck.draw_card())
+    dealer.get_point(deck.draw_card())
+    player.get_point(deck.draw_card())
+    dealer.get_point(deck.draw_card())
+    player.if_display(deck.draw_card())
+    player.if_display(deck.draw_card())
+    dealer.if_display(deck.draw_card())
+    dealer.if_display(deck.draw_card(),display=False)
+    while player.is_continue():
+        player.get_point(deck.draw_card())
+        if player.over_twenty_one():
+            print('21 を越えました')
+            print('あなたの負けです')
+            break
+
+    if not player.over_twenty_one():
+        dealer.display_suit_rank(2)
+        while dealer.is_continue():
+            dealer.get_point(deck.draw_card())
+
+        if dealer.over_twenty_one() or player.get_point() >= dealer.get_point():
+            print('あなたの勝ちです')
+        else:
+            print('あなたの負けです')
